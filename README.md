2703c66e54c5e611499f2283b1f5c9bd89042759
rehanhanar7 <rehanhanar7@gmail.com>
2021-10-02 13:48:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-mmdees
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-mmdees)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
